7aed443d96b01f4dad32b538b89ec8e1ee92019d
Parth Patel <parthpatel6037@gmail.com>
2023-04-07 22:46:13 +0530

    add example as input

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,24 +7,28 @@ function Form() {
   const [contentType, setContentType] = useState('');
   const [topic, setTopic] = useState('');
   const [audience, setAudience] = useState('');
-  const [numResponse, setNumResponse] = useState(0);
+  // const [numResponse, setNumResponse] = useState(0);
   const [tone, setTone] = useState('');
+  const [ref_Article, setrefArticle] = useState("");
   const [responseData, setResponseData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [showResults, setShowResults] = useState(false);
 
+  
   const handleSubmit = (event) => {
     event.preventDefault();
+    
     const data = {
       content_type: contentType,
       topic,
       audience,
-      num_responses: parseInt(numResponse),
       tone,
+      ref_Article
     };
-    
+      // num_responses: parseInt(numResponse),
+    // https://tmv96hl3i4.execute-api.us-east-1.amazonaws.com/dev/api/v1/content
     setLoading(true);
-    axios.post(`https://tmv96hl3i4.execute-api.us-east-1.amazonaws.com/dev/api/v1/content`,data, {
+    axios.post(`https://jlyakjs58d.execute-api.us-east-1.amazonaws.com/dev/api/v1/content`,data, {
       headers: { }
     })
       .then((response) => {
@@ -80,11 +84,15 @@ function Form() {
         <input type="text" id="audience" name="audience" value={audience} onChange={(e) => setAudience(e.target.value)} required />
       </div>
 
-      <div className="form-group">
-        <label htmlFor="num-responses">Number of Responses:</label>
-        <input type="number" id="num-responses" name="num-responses" value={numResponse} onChange={(e) => setNumResponse(e.target.value)} max="3" min="0" required />
-      </div>
+      {/* // <div className="form-group">
+      //   <label htmlFor="num-responses">Number of Responses:</label>
+      //   <input type="number" id="num-responses" name="num-responses" value={numResponse} onChange={(e) => setNumResponse(e.target.value)} max="3" min="0" required />
+      // </div> */}
 
+      <div className="form-group">
+        <label htmlFor="example">Example:</label>
+        <textarea id="example" name="example" value={ref_Article} onChange={(e) => setrefArticle(e.target.value.replace(/\s+/g, ' ').trim())} rows="10" cols="110" />
+      </div>
       <div className="form-group">
         <label htmlFor="tone">Tone:</label>
         <select id="tone" name="tone" value={tone} onChange={(e) => setTone(e.target.value)}>
